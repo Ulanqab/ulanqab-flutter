d35a78c1df6883492407f7ad0cd778e6a61ee0ae
JunaYa <ayac3j@gmail.com>
2021-02-11 01:00:54 +0800

    styles: chapter item

--- lib/components/ChapterItem.dart
+++ lib/components/ChapterItem.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ulanqab/model/Chapter.dart';
 
-final _borderRadius = BorderRadius.circular(16.0);
 
 class ChapterItem extends StatelessWidget {
   final Chapter chapter;
@@ -13,9 +12,9 @@ class ChapterItem extends StatelessWidget {
     return Material(
       color: Colors.white,
       child: Container(
-        height: 240.0,
+        height: 200.0,
         child: InkWell(
-          borderRadius: _borderRadius,
+          borderRadius: BorderRadius.circular(16.0),
           highlightColor: Colors.white60,
           splashColor: Colors.white70,
           onTap: () {
@@ -27,41 +26,63 @@ class ChapterItem extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  color: Colors.green,
-                  child: Padding(
-                    padding: EdgeInsets.all(8),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),//圆角半径为8
-                      child: Image.network(chapter.chapterPicture, width: 110, height: 180)
-                    ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),//圆角半径为8
+                    child: Image.network(chapter.chapterPicture, width: 112, height: 180)
                   ),
                 ),
                 Expanded(
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
                       Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                         Text(
-                          chapter.chapterTitle,
-                          maxLines: 1,
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black12,
-                          ),),
+                          Flexible(child:
+                            Text(
+                              chapter.chapterTitle,
+                              maxLines: 1,
+                              textAlign: TextAlign.left,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Flexible(child:
+                            Text(
+                              chapter.courseIndex,
+                              maxLines: 1,
+                              textAlign: TextAlign.left,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
                         ],
                       ),
                       Row(
                         crossAxisAlignment: CrossAxisAlignment.end,
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          Padding(padding: EdgeInsets.all(0),child: Image(image: AssetImage('assets/icons/icon_star_normal.png'), width: 36, height: 36,),),
-                          Padding(padding: EdgeInsets.all(0),child: Image(image: AssetImage('assets/icons/icon_star_normal.png'), width: 36, height: 36,),),
-                          Padding(padding: EdgeInsets.all(0),child: Image(image: AssetImage('assets/icons/icon_star_normal.png'), width: 36, height: 36,),),
+                          Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 0),child: Image(image: AssetImage('assets/icons/icon_star_active.png'), width: 36, height: 36,),),
+                          Padding(padding: EdgeInsets.fromLTRB(4, 0, 0, 0),child: Image(image: AssetImage('assets/icons/icon_star_active.png'), width: 36, height: 36,),),
+                          Padding(padding: EdgeInsets.fromLTRB(4, 0, 0, 0),child: Image(image: AssetImage('assets/icons/icon_star_normal.png'), width: 36, height: 36,),),
                         ],
                       )
                     ],
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:ulanqab/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(UlanqabApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
