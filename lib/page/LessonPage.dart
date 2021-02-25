61bfbcd1824aeebeb0087c1edcaef5c731016662
JunaYa <ayac3j@gmail.com>
2021-02-25 23:52:30 +0800

    feat: LearningScene navigator

--- lib/page/LessonPage.dart
+++ lib/page/LessonPage.dart
@@ -11,36 +11,41 @@ class LessonPage extends StatefulWidget {
   _LessonPage createState() => _LessonPage();
 }
 
-Widget _lessonItem() {
+Widget _lessonItem(context) {
   const coverUrl =
       'https://oss-sts-upload.yunshuxie.com/pic/thadm/material/2020/04/27/16/08/17/bca4817906974f74a7f7e2016e5b8309/ea4c2dd9-ab73-4579-add2-fd897305a869.png';
-  return Container(
-    width: 160,
-    height: 50,
-    margin: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
-    padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
-    decoration: new BoxDecoration(
-        color: Colors.black,
-        borderRadius: new BorderRadius.all(new Radius.circular(16.0)),
-        image: new DecorationImage(image: NetworkImage(coverUrl), fit: BoxFit.cover)),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Padding(
-          padding: EdgeInsets.all(0),
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(0, 0, 0, 16.0),
-            child: Text('Lesson 标题',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 18.0,
-              color: Colors.black,
-              fontWeight: FontWeight.w500,
-            ),),),
-        ),
-        StarLayout(nums: 2, align: 1,),
-      ],
+  return InkWell(
+    onTap: () {
+      Navigator.pushNamed(context, '/LearningScene');
+    },
+    child: Container(
+      width: 160,
+      height: 50,
+      margin: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
+      padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
+      decoration: new BoxDecoration(
+          color: Colors.black,
+          borderRadius: new BorderRadius.all(new Radius.circular(16.0)),
+          image: new DecorationImage(image: NetworkImage(coverUrl), fit: BoxFit.cover)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(0),
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 16.0),
+              child: Text('Lesson 标题',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+              ),),),
+          ),
+          StarLayout(nums: 2, align: 1,),
+        ],
+      ),
     ),
   );
 }
@@ -89,9 +94,11 @@ class _LessonPage extends State<LessonPage> {
             Expanded(child: ListView(
               scrollDirection: Axis.horizontal,
               children: [
-                _lessonItem(),
-                _lessonItem(),
-                _lessonItem(),
+                _lessonItem(context),
+                _lessonItem(context),
+                _lessonItem(context),
+                _lessonItem(context),
+                _lessonItem(context),
               ],
             )),
           ],
