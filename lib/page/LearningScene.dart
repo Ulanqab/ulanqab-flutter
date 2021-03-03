57b982e97c2ddc70c14f5a26e8009a0d74c1e86d
JunaYa <ayac3j@gmail.com>
2021-03-03 23:49:19 +0800

    feat: com loading

--- lib/page/LearningScene.dart
+++ lib/page/LearningScene.dart
@@ -6,17 +6,61 @@ class LearningScene extends StatefulWidget {
   _LearningScene createState() => _LearningScene();
 }
 
-Widget _ComLoading() {
+Widget _ComLoading(context) {
+  final size = MediaQuery.of(context).size;
+  final width = size.width;
+  final height = size.height;
+  final sceneRatio = 375 / 675;
+  final ratio = width / height;
+
+  var w = width;
+  var h = height;
+
+  if (sceneRatio > ratio) {
+    w = width / ratio;
+    h = height * ratio;
+  } else {
+    w = width / ratio;
+    h = height;
+  }
+
+  print('w++++');
+  print(w);
+  print('h----');
+  print(h);
+
   return Container(
-    width: 100,
-    height: 100,
+    width: 675,
+    height: 375,
+    alignment: Alignment.center,
     decoration: new BoxDecoration(
+      color: Colors.black,
       image: new DecorationImage(
-        image: new NetworkImage('https://cdn.yunshuxie.com/m/page/learning_scene/img/loading_bg.png'),
-        fit: BoxFit.contain
-      ),
+          image: new NetworkImage(
+              'https://cdn.yunshuxie.com/m/page/learning_scene/img/loading_bg.png'),
+          fit: BoxFit.cover),
     ),
   );
+}
+
+Widget _ButtonBack(context) {
+  return Positioned(
+    left: 16,
+    top: 16,
+    child: InkWell(
+      child: Image(
+        image: AssetImage('assets/icons/icon_back.png'),
+        width: 32,
+        height: 32,
+      ),
+      onTap: () {
+        Navigator.pop(context);
+        SystemChrome.setPreferredOrientations([
+          DeviceOrientation.portraitUp,
+          DeviceOrientation.portraitDown,
+        ]);
+    },
+  ));
 }
 
 class _LearningScene extends State<LearningScene> {
@@ -29,11 +73,10 @@ class _LearningScene extends State<LearningScene> {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: ListView(
-        children: [
-          _ComLoading(),
-        ],
-      ),
+      body: Stack(children: [
+        _ButtonBack(context),
+        Center(child: _ComLoading(context),),
+      ],),
     );
   }
 }
